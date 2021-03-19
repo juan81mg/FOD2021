a64c4c07973e233b87b1133eaeddcc896a26ed74
Juan Manuel Gil <juan81mg@gmail.com>
2021-03-19 14:56:45 -0300

    final

--- practica_01/01.pas
+++ practica_01/01.pas
@@ -1,7 +1,8 @@
-{Realizar un algoritmo que cree un archivo de números enteros no ordenados y permita
-incorporar datos al archivo. Los números son ingresados desde teclado. El nombre del
-archivo debe ser proporcionado por el usuario desde teclado. La carga finaliza cuando
-se ingrese el número 30000, que no debe incorporarse al archivo.}
+{Realizar un algoritmo que cree un archivo de números enteros no ordenados 
+y permita incorporar datos al archivo. 
+Los números son ingresados desde teclado. 
+El nombre del archivo debe ser proporcionado por el usuario desde teclado. 
+La carga finaliza cuando se ingrese el número 30000, que no debe incorporarse al archivo.}
 
 program p01e01;
 
@@ -10,16 +11,28 @@ type
 	
 var
 	numeros: arc_num;
+	nom_fis: string[12];
 	num: integer;
 
 begin
 	
-	assign(numeros, '01.dat');
+	writeln('Ingrese el nombre del archivo fisico');
+	read(nom_fis);
 	
+	assign(numeros, nom_fis);
 	rewrite(numeros);
+
+	writeln('Agregue un numero entero (3000 para salir)');
+	read(num);
 	
-	
-	
+	while(num <> 3000) do begin
+		write(numeros, num);
+		
+		writeln('Agregue un numero entero (3000 para salir)');
+		read(num);
+		end;
+
+	writeln('Salio del programa');
 	close(numeros);
 
 end.
