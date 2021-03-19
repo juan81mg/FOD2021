ee1ab8e9c02fa3d73f50087d23aec74026561932
Juan Manuel Gil <juan81mg@gmail.com>
2021-03-19 14:19:23 -0300

    Create 01.pas

---
+++ practica_01/01.pas
@@ -0,0 +1,5 @@
+{Realizar un algoritmo que cree un archivo de números enteros no ordenados y permita
+incorporar datos al archivo. Los números son ingresados desde teclado. El nombre del
+archivo debe ser proporcionado por el usuario desde teclado. La carga finaliza cuando
+se ingrese el número 30000, que no debe incorporarse al archivo.}
+
