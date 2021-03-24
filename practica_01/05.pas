df42ba45925a355d00130eac115ab723be25d550
Juan Manuel Gil <juan81mg@gmail.com>
2021-03-24 18:48:20 -0300

    05 - final

--- practica_01/05.pas
+++ practica_01/05.pas
@@ -35,7 +35,61 @@ type
 
 var
     celulares: archivo;
-    op: integer;
+    op, o: integer;
+
+
+procedure exportarTXT(var celulares: archivo);
+    var
+        cel: celular;
+        tex: Text;
+    begin
+        reset(celulares);
+        Assign(tex, 'celulares.txt');
+        Rewrite(tex);
+		while not eof(celulares)do begin
+			read(celulares, cel);
+		  	with cel do begin
+                writeln(tex, ' ', cod, ' ', precio, ' ', marca_nombre);
+                writeln(tex, ' ', stk_dis, ' ', stk_min, ' ', descripcion);
+                end;
+			end;
+        close(tex);
+        close(celulares);
+    end;
+
+procedure buscarDescripcion(var celulares: archivo);
+    var
+        cel: celular;
+        buscar: string[24];
+    begin
+        reset(celulares);
+        writeln('Ingrese la cadena a buscar en la descripcion');
+        readln(buscar);
+        buscar := ' ' + buscar;
+        while not eof (celulares) do begin
+            read (celulares, cel);
+            if (cel.descripcion = buscar) then begin
+                writeln('cod: ', cel.cod, ' precio: ', cel.precio, ' marca y nombre: ', cel.marca_nombre);
+                writeln('stok disponible: ', cel.stk_dis, ' stock minimo: ', cel.stk_min, ' descripcion: ', cel.descripcion);
+                end;
+            end;
+        close(celulares);
+    end;
+
+procedure listarMinimos(var celulares: archivo);
+    var
+        cel: celular;
+    begin
+        reset(celulares);
+        while not eof (celulares) do begin
+            read (celulares, cel);
+            if (cel.stk_dis < cel.stk_min) then begin
+                writeln('cod: ', cel.cod, ' precio: ', cel.precio, ' marca y nombre: ', cel.marca_nombre);
+                writeln('stok disponible: ', cel.stk_dis, ' stock minimo: ', cel.stk_min, ' descripcion: ', cel.descripcion);
+                end;
+            end;
+        close(celulares);
+    end;
 
 procedure crearArchivo(var celulares: archivo);
 	var
@@ -43,25 +97,19 @@ procedure crearArchivo(var celulares: archivo);
         carga: Text;
 	begin
 		rewrite(celulares);
-
         assign(carga, 'carga.txt');
         reset(carga);
-
         while not eof (carga)do begin
             readln(carga, cel.cod, cel.precio, cel.marca_nombre);
             readln(carga, cel.stk_dis, cel.stk_min, cel.descripcion);
-
             writeln('cod: ', cel.cod, ' precio: ', cel.precio, ' marca y nombre: ', cel.marca_nombre);
             writeln('stok disponible: ', cel.stk_dis, ' stock minimo: ', cel.stk_min, ' descripcion: ', cel.descripcion);
-
             write(celulares, cel);
-
         end;
-
         close(carga);
         close(celulares);
 	end;
-    
+
 procedure nombreArchivo(var celulares: archivo);
 	var 
 		nom_fis: string[12];
@@ -71,15 +119,28 @@ procedure nombreArchivo(var celulares: archivo);
 		assign(celulares, nom_fis);
 	end;
 
+
+procedure submenu(var o: integer);
+	begin
+		writeln('');
+		writeln(' --- Opciones de archivo --- ');
+		writeln('');
+		writeln('(1) Listar celulares con menor stock al minimo');
+        writeln('(2) Buscar una descripcion');
+        writeln('(3) Exportar a TXT');
+		writeln('');
+		writeln('(0) Salir');
+		writeln('');
+		write('Opcion --->> ');
+		readln(o);
+	end;
+
 procedure menu(var op: integer);
 	begin
 		writeln('');
 		writeln(' --- Opciones de archivo --- ');
 		writeln('');
 		writeln('(1) Crear un archivo binario de celulares');
-		writeln('(2)');
-        writeln('(3)');
-        writeln('(4)');
 		writeln('');
 		writeln('(0) Salir');
 		writeln('');
@@ -100,9 +161,32 @@ begin
             1: begin
                 nombreArchivo(celulares);
                 crearArchivo(celulares);
-                end;
-			2: begin
-                end;
+                o:= -1;
+                while (o <> 0) do begin
+                    submenu(o);
+                    case o of
+                        0: begin
+                            writeln();
+                            writeln('Salio del programa');
+                            writeln();
+                            op := 0;
+                            end;
+                        1: listarMinimos(celulares);
+                        2: buscarDescripcion(celulares);
+                        3: exportarTXT(celulares);
+                        else begin
+                            writeln();
+                            writeln('Ingrese una opcion correcta !!!');
+                            writeln();
+                            end;
+                        end;
+                    end;
+                end
+			else begin
+				writeln();
+				writeln('Ingrese una opcion correcta !!!');
+				writeln();
+				end;
             end;
         end;
 end.
